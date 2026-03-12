f14b145303ee073217f779de86b2c497835e4673
BaBaBuaB <arm5052sueksa@gmail.com>
2026-03-12 14:26:24 +0700

    Fix1

--- Assets/Platformer/Scripts/Components/PlatformerHUD.cs
+++ Assets/Platformer/Scripts/Components/PlatformerHUD.cs
@@ -1,4 +1,5 @@
 using Blocks.Gameplay.Core;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Blocks.Gameplay.Platformer
@@ -10,9 +11,15 @@ namespace Blocks.Gameplay.Platformer
     public class PlatformerHUD : CoreHUD
     {
         #region Fields & Properties
-
+        
         private Label m_CoinsLabel;
+        
+        private ProgressBar m_PlayerJetpackFuelBar;
+        private VisualElement m_PlayerJetpackFuelBarFill;
 
+// You can pick any color you like. Here we use Orange. (You will need to import using.UnityEngine;)
+        private static readonly Color k_JetpackFuelBarColor = new Color(1.0f, 0.647f, 0.0f, 0.75f);
+        
         #endregion
 
         #region Protected Methods
@@ -25,6 +32,17 @@ namespace Blocks.Gameplay.Platformer
         {
             base.QueryHUDElements(root);
             m_CoinsLabel = root.Q<Label>("coins-label");
+            
+            m_PlayerJetpackFuelBar = root.Q<ProgressBar>("player-jetpack-fuel-bar");
+            if (m_PlayerJetpackFuelBar != null)
+            {
+                // Optional: Set specific color for the fill
+                m_PlayerJetpackFuelBarFill = m_PlayerJetpackFuelBar.Q<VisualElement>(null, "unity-progress-bar__progress");
+                if (m_PlayerJetpackFuelBarFill != null)
+                {
+                    m_PlayerJetpackFuelBarFill.style.backgroundColor = k_JetpackFuelBarColor;
+                }
+            }
         }
 
         /// <summary>
@@ -39,6 +57,13 @@ namespace Blocks.Gameplay.Platformer
                     if (m_CoinsLabel != null)
                     {
                         m_CoinsLabel.text = $"Coins: {payload.currentValue:F0}";
+                    }
+                    break;
+                case "JetpackFuel":
+                    if (m_PlayerJetpackFuelBar != null)
+                    {
+                        m_PlayerJetpackFuelBar.highValue = payload.maxValue;
+                        m_PlayerJetpackFuelBar.value = payload.currentValue;
                     }
                     break;
             }
